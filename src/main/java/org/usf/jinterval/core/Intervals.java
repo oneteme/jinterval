10daa96d05cc275a301eb73a60e3e1cf25456243
usf <usf.alami@gmail.com>
2021-05-21 00:28:57 +0200

    intevals_tests

--- src/main/java/org/usf/jinterval/core/Intervals.java
+++ src/main/java/org/usf/jinterval/core/Intervals.java
@@ -4,6 +4,7 @@ import static org.usf.jinterval.core.IntervalCollector.largestInterval;
 import static org.usf.jinterval.core.IntervalCollector.smallestInterval;
 import static org.usf.jinterval.partition.multiple.Partitions.intervalParts;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.BiConsumer;
@@ -25,142 +26,163 @@ public final class Intervals {
 	private static final IntPredicate DIRTY_INTERVALS_FILTER   = i-> i != 1;
 	
 	
-	public static <T extends Comparable<? super T>> Optional<Interval<T>> minInterval(List<? extends Interval<T>> intervals) {
+	public static <T extends Comparable<? super T>> Optional<Interval<T>> minInterval(Collection<? extends Interval<T>> intervals) {
 
 		return intervals.stream().collect(smallestInterval());
 	}
 
-	public static <T extends Comparable<? super T>> Optional<Interval<T>> maxInterval(List<? extends Interval<T>> intervals) {
+	public static <T extends Comparable<? super T>> Optional<Interval<T>> maxInterval(Collection<? extends Interval<T>> intervals) {
 
 		return intervals.stream().collect(largestInterval());
 	}
 	
-	public static <T extends Comparable<? super T>> boolean isMissingIntervals(List<? extends Interval<T>> intervals) {
-
-		return testIf(null, null, intervals, MISSING_INTERVALS_FILTER);
-	}
-	public static <T extends Comparable<? super T>> boolean isMissingIntervals(T start, T exclusifEnd, List<? extends Interval<T>> intervals) {
-
-		return testIf(start, exclusifEnd, intervals, MISSING_INTERVALS_FILTER);
-	}
-
-	public static <T extends Comparable<? super T>> void requiredNotMissingIntervals(List<? extends Interval<T>> intervals) {
+	/******** testIf ********/
 	
-		throwsIf(null, null, intervals, MISSING_INTERVALS_FILTER, "missing interval");
-	}
-	public static <T extends Comparable<? super T>> void requiredNotMissingIntervals(T start, T exclusifEnd, List<? extends Interval<T>> intervals) {
+	public static <T extends Comparable<? super T>> boolean isMissingIntervals(boolean reversed, Collection<? extends Interval<T>> intervals) {
 
-		throwsIf(start, exclusifEnd, intervals, MISSING_INTERVALS_FILTER, "missing interval");
+		return testIf(reversed, null, null, intervals, MISSING_INTERVALS_FILTER);
 	}
-	
-	public  static <T extends Comparable<? super T>> boolean isOverlapIntervals(List<? extends Interval<T>> intervals) {
+	public static <T extends Comparable<? super T>> boolean isMissingIntervals(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals) {
 
-		return testIf(null, null, intervals, OVERLAP_INTERVALS_FILTER);
+		return testIf(reversed, start, exclusifEnd, intervals, MISSING_INTERVALS_FILTER);
 	}
 
-	public  static <T extends Comparable<? super T>> void requiredNotOverlapIntervals(List<? extends Interval<T>> intervals) {
+	public  static <T extends Comparable<? super T>> boolean isOverlappingIntervals(boolean reversed, Collection<? extends Interval<T>> intervals) {
 
-		throwsIf(null, null, intervals, OVERLAP_INTERVALS_FILTER, "overlaping interval");
+		return testIf(reversed, null, null, intervals, OVERLAP_INTERVALS_FILTER);
+	}
+	public  static <T extends Comparable<? super T>> boolean isOverlappingIntervals(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals) {
+
+		return testIf(reversed, start, exclusifEnd, intervals, OVERLAP_INTERVALS_FILTER);
 	}
 	
-	public  static <T extends Comparable<? super T>> boolean isLinkedIntervals(List<? extends Interval<T>> intervals) {
+	public  static <T extends Comparable<? super T>> boolean isMissingOrOverlappingIntervals(boolean reversed, Collection<? extends Interval<T>> intervals) {
+
+		return testIf(reversed, null, null, intervals, DIRTY_INTERVALS_FILTER);
+	}
+	public  static <T extends Comparable<? super T>> boolean isMissingOrOverlappingIntervals(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals) {
+
+		return testIf(reversed, start, exclusifEnd, intervals, DIRTY_INTERVALS_FILTER);
+	}
+
+	/******** throwsIf ********/
+
+	public static <T extends Comparable<? super T>> void requiredNotMissingIntervals(boolean reversed, Collection<? extends Interval<T>> intervals) {
+	
+		throwsIf(reversed, null, null, intervals, MISSING_INTERVALS_FILTER, "missing interval");
+	}
+	public static <T extends Comparable<? super T>> void requiredNotMissingIntervals(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals) {
+
+		throwsIf(reversed, start, exclusifEnd, intervals, MISSING_INTERVALS_FILTER, "missing interval");
+	}
+
+	public  static <T extends Comparable<? super T>> void requiredNotOverlappingIntervals(boolean reversed, Collection<? extends Interval<T>> intervals) {
+
+		throwsIf(reversed, null, null, intervals, OVERLAP_INTERVALS_FILTER, "overlaping interval");
+	}
+	public  static <T extends Comparable<? super T>> void requiredNotOverlappingIntervals(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals) {
+
+		throwsIf(reversed, start, exclusifEnd, intervals, OVERLAP_INTERVALS_FILTER, "overlaping interval");
+	}
+	
+	public  static <T extends Comparable<? super T>> void requiredNotMissingAndOverlappingIntervals(boolean reversed, Collection<? extends Interval<T>> intervals) {
+
+		throwsIf(reversed, null, null, intervals, DIRTY_INTERVALS_FILTER, "mismatch interval");
+	}
+	public  static <T extends Comparable<? super T>> void requiredNotMissingAndOverlappingIntervals(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals) {
+
+		throwsIf(reversed, start, exclusifEnd, intervals, DIRTY_INTERVALS_FILTER, "mismatch interval");
+	}
+
+	/******** filter ********/
+
+	public static <T extends Comparable<? super T>> List<Interval<T>> missingIntervals(boolean reversed, Collection<? extends Interval<T>> intervals) {
+
+		return filter(reversed, null, null, intervals, MISSING_INTERVALS_FILTER, ImmutableInterval::new, Collectors.toList());
+	}
+	public static <T extends Comparable<? super T>> List<Interval<T>> missingIntervals(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals) {
 		
-		return !testIf(null, null, intervals, DIRTY_INTERVALS_FILTER);
+		return filter(reversed, start, exclusifEnd, intervals, MISSING_INTERVALS_FILTER, ImmutableInterval::new, Collectors.toList());
 	}
-	public  static <T extends Comparable<? super T>> boolean isLinkedIntervals(T start, T exclusifEnd, List<? extends Interval<T>> intervals) {
+	
+	public static <T extends Comparable<? super T>> List<Interval<T>> overlappingIntervals(boolean reversed, Collection<? extends Interval<T>> intervals) {
+
+		return filter(reversed, null, null, intervals, OVERLAP_INTERVALS_FILTER, ImmutableInterval::new, Collectors.toList());
+	}
+	public static <T extends Comparable<? super T>> List<Interval<T>> overlappingIntervals(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals) {
+
+		return filter(reversed, start, exclusifEnd, intervals, OVERLAP_INTERVALS_FILTER, ImmutableInterval::new, Collectors.toList());
+	}
+	
+	public static <T extends Comparable<? super T>> List<Interval<T>> missingAndOverlappingIntervals(boolean reversed, Collection<? extends Interval<T>> intervals) {
+
+		return filter(reversed, null, null, intervals, DIRTY_INTERVALS_FILTER, ImmutableInterval::new, Collectors.toList());
+	}
+	public static <T extends Comparable<? super T>> List<Interval<T>> missingAndOverlappingIntervals(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals) {
+
+		return filter(reversed, start, exclusifEnd, intervals, DIRTY_INTERVALS_FILTER, ImmutableInterval::new, Collectors.toList());
+	}
+	
+	/******** collect ********/
+	
+	public static <T extends Comparable<? super T>, I, R> R collectMissingIntervals(boolean reversed, Collection<? extends Interval<T>> intervals, BiFunction<T, T, I> fn, Collector<I, ?, R> collector) {
+
+		return filter(reversed, null, null, intervals, MISSING_INTERVALS_FILTER, fn, collector);
+	}
+	public static <T extends Comparable<? super T>, I, R> R collectMissingIntervals(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals, BiFunction<T, T, I> fn, Collector<I, ?, R> collector) {
 		
-		return !testIf(start, exclusifEnd, intervals, DIRTY_INTERVALS_FILTER);
-	}
-
-	public static <T extends Comparable<? super T>> void requiredLinkedIntervals(List<? extends Interval<T>> intervals) {
-
-		throwsIf(null, null, intervals, DIRTY_INTERVALS_FILTER, "mismatch interval");
-	}
-
-	public static <T extends Comparable<? super T>> void requiredLinkedIntervals(T start, T exclusifEnd, List<? extends Interval<T>> intervals) {
-
-		throwsIf(start, exclusifEnd, intervals, DIRTY_INTERVALS_FILTER, "mismatch interval");
-	}
-
-
-	public static <T extends Comparable<? super T>> List<Interval<T>> missingIntervals(List<? extends Interval<T>> intervals) {
-
-		return filter(null, null, intervals, MISSING_INTERVALS_FILTER, ImmutableInterval::new, Collectors.toList());
-	}
-	public static <T extends Comparable<? super T>> List<Interval<T>> missingIntervals(T start, T exclusifEnd, List<? extends Interval<T>> intervals) {
-		
-		return filter(start, exclusifEnd, intervals, MISSING_INTERVALS_FILTER, ImmutableInterval::new, Collectors.toList());
+		return filter(reversed, start, exclusifEnd, intervals, MISSING_INTERVALS_FILTER, fn, collector);
 	}
 	
-	public static <T extends Comparable<? super T>> List<Interval<T>> overlapIntervals(List<? extends Interval<T>> intervals) {
+	public static <T extends Comparable<? super T>, I, R> R collectOverlappingIntervals(boolean reversed, Collection<? extends Interval<T>> intervals, BiFunction<T, T, I> fn, Collector<I, ?, R> collector) {
 
-		return filter(null, null, intervals, OVERLAP_INTERVALS_FILTER, ImmutableInterval::new, Collectors.toList());
+		return filter(reversed, null, null, intervals, OVERLAP_INTERVALS_FILTER, fn, collector);
 	}
-	
-	public static <T extends Comparable<? super T>> List<Interval<T>> missingAndOverlapIntervals(List<? extends Interval<T>> intervals) {
+	public static <T extends Comparable<? super T>, I, R> R collectOverlappingIntervals(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals, BiFunction<T, T, I> fn, Collector<I, ?, R> collector) {
 
-		return filter(null, null, intervals, DIRTY_INTERVALS_FILTER, ImmutableInterval::new, Collectors.toList());
-	}
-	public static <T extends Comparable<? super T>> List<Interval<T>> missingAndOverlapIntervals(T start, T exclusifEnd, List<? extends Interval<T>> intervals) {
-
-		return filter(start, exclusifEnd, intervals, DIRTY_INTERVALS_FILTER, ImmutableInterval::new, Collectors.toList());
-	}
-	
-	public static <T extends Comparable<? super T>, I extends Interval<T>, R> R collectMissingIntervals(List<? extends Interval<T>> intervals, BiFunction<T, T, I> fn, Collector<I, ?, R> collector) {
-
-		return filter(null, null, intervals, MISSING_INTERVALS_FILTER, fn, collector);
-	}
-	public static <T extends Comparable<? super T>, I extends Interval<T>, R> R collectMissingIntervals(T start, T exclusifEnd, List<? extends Interval<T>> intervals, BiFunction<T, T, I> fn, Collector<I, ?, R> collector) {
-		
-		return filter(start, exclusifEnd, intervals, MISSING_INTERVALS_FILTER, fn, collector);
-	}
-	
-	public static <T extends Comparable<? super T>, I extends Interval<T>, R> R collectOverlapIntervals(List<? extends Interval<T>> intervals, BiFunction<T, T, I> fn, Collector<I, ?, R> collector) {
-
-		return filter(null, null, intervals, OVERLAP_INTERVALS_FILTER, fn, collector);
+		return filter(reversed, start, exclusifEnd, intervals, OVERLAP_INTERVALS_FILTER, fn, collector);
 	}
 
-	public static <T extends Comparable<? super T>, I extends Interval<T>, R> R collectDirtyIntervals(List<? extends Interval<T>> intervals, BiFunction<T, T, I> fn, Collector<I, ?, R> collector) {
+	public static <T extends Comparable<? super T>, I, R> R collectMissingAndOverlappingIntervals(boolean reversed, Collection<? extends Interval<T>> intervals, BiFunction<T, T, I> fn, Collector<I, ?, R> collector) {
 
-		return filter(null, null, intervals, DIRTY_INTERVALS_FILTER, fn, collector);
+		return filter(reversed, null, null, intervals, DIRTY_INTERVALS_FILTER, fn, collector);
 	}
-	public static <T extends Comparable<? super T>, I extends Interval<T>, R> R collectDirtyIntervals(T start, T exclusifEnd, List<? extends Interval<T>> intervals, BiFunction<T, T, I> fn, Collector<I, ?, R> collector) {
+	public static <T extends Comparable<? super T>, I, R> R collectMissingAndOverlappingIntervals(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals, BiFunction<T, T, I> fn, Collector<I, ?, R> collector) {
 
-		return filter(start, exclusifEnd, intervals, DIRTY_INTERVALS_FILTER, fn, collector);
+		return filter(reversed, start, exclusifEnd, intervals, DIRTY_INTERVALS_FILTER, fn, collector);
 	}
 
-	public static <T extends Comparable<? super T>, I extends Interval<T>, C, R> R filter(T start, T exclusifEnd, List<? extends Interval<T>> intervals, IntPredicate intervalCount, BiFunction<T, T, I> fn, Collector<I, C, R> collector) {
+	public static <T extends Comparable<? super T>, I, C, R> R filter(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals, IntPredicate intervalCount, BiFunction<T, T, I> fn, Collector<I, C, R> collector) {
 
 		C list = collector.supplier().get();
-		acceptIf(start, exclusifEnd, intervals, intervalCount, 
+		acceptIf(reversed, start, exclusifEnd, intervals, intervalCount, 
 				(sp, ep)-> collector.accumulator().accept(list, fn.apply(sp, ep)));
 		return collector.finisher().apply(list);
 	}
 	
-	private static <T extends Comparable<? super T>> boolean testIf(T start, T exclusifEnd, List<? extends Interval<T>> intervals, IntPredicate nIntervalPredicate) {
+	private static <T extends Comparable<? super T>> boolean testIf(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals, IntPredicate nIntervalPredicate) {
 		
 		try {
-			throwsIf(start, exclusifEnd, intervals, nIntervalPredicate, null);
+			throwsIf(reversed, start, exclusifEnd, intervals, nIntervalPredicate, null);
 		} catch (IntervalMismatchException e) {
 			return true;
 		}
 		return false;
 	}
 	
-	private static <T extends Comparable<? super T>> void throwsIf(T start, T exclusifEnd, List<? extends Interval<T>> intervals, IntPredicate nIntervalPredicate, String message) {
+	private static <T extends Comparable<? super T>> void throwsIf(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals, IntPredicate nIntervalPredicate, String message) {
 
-		acceptIf(start, exclusifEnd, intervals, nIntervalPredicate, 
+		acceptIf(reversed, start, exclusifEnd, intervals, nIntervalPredicate, 
 				(s,e)-> {throw new IntervalMismatchException(message);});
 	}
 
-	public static <T extends Comparable<? super T>> void acceptIf(T start, T exclusifEnd, List<? extends Interval<T>> intervals, IntPredicate nIntervalPredicate, BiConsumer<T,T> consumer) {
+	public static <T extends Comparable<? super T>> void acceptIf(boolean reversed, T start, T exclusifEnd, Collection<? extends Interval<T>> intervals, IntPredicate nIntervalPredicate, BiConsumer<T,T> consumer) {
 
-		intervalParts(intervals, start, exclusifEnd, (sp, ep, models, min)->{
+		intervalParts(reversed, intervals, start, exclusifEnd, (sp, ep, models, min)->{
 			if(nIntervalPredicate.test((int)models.count())) {
 				consumer.accept(sp, ep);
 			}
 		});
 	}
-	
 	
 }
--- src/test/java/org/usf/jinterval/core/IntervalsTest.java
+++ src/test/java/org/usf/jinterval/core/IntervalsTest.java
@@ -1,36 +1,98 @@
 package org.usf.jinterval.core;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.usf.jinterval.Utils.assertExceptionMsg;
+import static org.usf.jinterval.Utils.intervals;
+import static org.usf.jinterval.core.Intervals.*;
 
-import java.time.LocalDate;
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
+import org.usf.jinterval.core.exception.IntervalMismatchException;
 
 class IntervalsTest {
 	
 	@Test
-	void testIsMissingIntervals() {
+	void testMissingIntervals() {
 		
-		var v1 = Arrays.asList(interval(LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 2)));
-		assertFalse(Intervals.isMissingIntervals(v1));
-		
-
-		var v2 = Arrays.asList(interval(LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 10)), 
-				interval(LocalDate.of(2020, 1, 5), LocalDate.of(2020, 1, 20)));
-		assertFalse(Intervals.isMissingIntervals(v2));
-		
-
-		var v3 = Arrays.asList(interval(LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 10)), 
-				interval(LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 20)));
-		assertTrue(Intervals.isMissingIntervals(v3));
-		
+		testMissingIntervals(false, emptyList(), emptyList());
+		testMissingIntervals(false, intervals(-1,3, 5,7, 10,15), intervals(3,5, 7,10));
+		testMissingIntervals(true, intervals(-1,3, 5,7, 4,15), intervals(3,4, 15,-1));
+		testMissingIntervals(true, intervals(-1,3, 5,7, 4,15, 3,5, 15,1), emptyList());
 	}
 	
-	private static <T extends Comparable<? super T>> Interval<T> interval(T start, T exludedEnd) {
-		
-		return new ImmutableInterval<T>(start, exludedEnd);
-	}
+	@Test
+	void testOverlapingIntervals() {
 
+		testOverlapingIntervals(false, emptyList(), emptyList());
+		testOverlapingIntervals(false, intervals(-1,3, 5,7, 10,15), emptyList());
+		testOverlapingIntervals(true, intervals(-1,3, 5,7, 4,15), intervals(5,7));
+		testOverlapingIntervals(true, intervals(-1,3, 5,7, 4,15, 3,5, 15,1), intervals(-1,1, 4,5, 5,7));
+	}
+	
+
+	@Test
+	void testMissingAngOverlapingIntervals() {
+
+		testMissingAngOverlapingIntervals(false, emptyList(), emptyList());
+		testMissingAngOverlapingIntervals(false, intervals(-1,3, 5,7, 10,15), intervals(3,5, 7,10));
+		testMissingAngOverlapingIntervals(true, intervals(-1,3, 5,7, 4,15), intervals(3,4, 5,7, 15,-1));
+		testMissingAngOverlapingIntervals(true, intervals(-1,3, 5,7, 4,15, 3,5, 15,1), intervals(-1,1, 4,5, 5,7));
+	}
+	
+	private void testMissingIntervals(boolean inverted, Collection<? extends Interval<Integer>> intervals, Collection<? extends Interval<Integer>> expected) {
+		
+		assertNotEquals(expected.isEmpty(), isMissingIntervals(inverted, intervals));
+		assertArrayEquals(expected.toArray(), missingIntervals(inverted, intervals).toArray());
+		if(expected.isEmpty()) {			
+			assertDoesNotThrow(()-> requiredNotMissingIntervals(inverted, intervals));
+		}
+		else {
+			assertExceptionMsg(IntervalMismatchException.class, ()-> requiredNotMissingIntervals(inverted, intervals), "missing interval");
+		}
+		assertArrayEquals(expected.toArray(), collectMissingIntervals(inverted, intervals, IntervalsTest::newInterval, Collectors.toList()).toArray());
+	}
+	
+	private void testOverlapingIntervals(boolean inverted, Collection<? extends Interval<Integer>> intervals, Collection<? extends Interval<Integer>> expected) {
+		
+		assertNotEquals(expected.isEmpty(), isOverlappingIntervals(inverted, intervals));
+		assertArrayEquals(expected.toArray(), overlappingIntervals(inverted, intervals).toArray());
+		if(expected.isEmpty()) {
+			assertDoesNotThrow(()-> requiredNotOverlappingIntervals(inverted, intervals));
+		}
+		else {
+			assertExceptionMsg(IntervalMismatchException.class, ()-> requiredNotOverlappingIntervals(inverted, intervals), "overlaping interval");
+		}
+		assertArrayEquals(expected.toArray(), collectOverlappingIntervals(inverted, intervals, IntervalsTest::newInterval, Collectors.toList()).toArray());
+	}
+	
+	private void testMissingAngOverlapingIntervals(boolean inverted, Collection<? extends Interval<Integer>> intervals, Collection<? extends Interval<Integer>> expected) {
+		
+		assertNotEquals(expected.isEmpty(), isMissingOrOverlappingIntervals(inverted, intervals));
+		assertArrayEquals(expected.toArray(), missingAndOverlappingIntervals(inverted, intervals).toArray());
+		if(expected.isEmpty()) {			
+			assertDoesNotThrow(()-> requiredNotMissingAndOverlappingIntervals(inverted, intervals));
+		}
+		else {
+			assertExceptionMsg(IntervalMismatchException.class, ()-> requiredNotMissingAndOverlappingIntervals(inverted, intervals), "mismatch interval");
+		}
+		assertArrayEquals(expected.toArray(), collectMissingAndOverlappingIntervals(inverted, intervals, IntervalsTest::newInterval, Collectors.toList()).toArray());
+	}
+	
+	
+	
+	private static <T extends Comparable<? super T>> Interval<T> newInterval(T start, T end) {
+	
+		return new Interval<T>() {
+			@Override
+			public T getStart() {return start;}
+			@Override
+			public T getExclusifEnd() {return end;}
+		};
+	}
+	
 }

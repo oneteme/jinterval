a7787c165ebd4e57b970abafc8855fd8f9fb8ee8
u <usf.alami@gmail.com>
2022-04-10 03:17:17 +0200

    edit

--- src/main/java/org/usf/java/jinterval/partition/single/SingleModelPartition.java
+++ src/main/java/org/usf/java/jinterval/partition/single/SingleModelPartition.java
@@ -5,6 +5,8 @@ import static java.util.stream.Collectors.toList;
 
 import java.time.Instant;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -27,10 +29,29 @@ public final class SingleModelPartition<M> {
 		
 		return new SingleModelGroupPartition<>(start, exclusifEnd, step, partitions.stream()
 		.collect(groupingBy(SingleModelPart::getModel)).entrySet().stream()
-		.map(e-> new SingleModelGroupPart<>(e.getKey(), e.getValue().stream()
-				.map(p-> new int[] {p.startIndex, p.endExclusiveIndex})
-				.toArray(int[][]::new)))
+		.map(e-> new SingleModelGroupPart<>(e.getKey(), chainParts(e.getValue())))
 		.collect(toList()));
+	}
+	
+	static final <M> int[][] chainParts(List<SingleModelPart<M>> parts){ //same model
+		var arr = new LinkedList<int[]>();
+		int i = 1;
+		while(i<parts.size()) {
+			var prv = parts.get(i-1);
+			var nxt = parts.get(i);
+			if(prv.getEndExclusiveIndex() == nxt.getStartIndex()) {
+				arr.add(new int[] {prv.getStartIndex(), nxt.getEndExclusiveIndex()});
+				i+=2;
+			}
+			else {
+				arr.add(new int[] {prv.getStartIndex(), prv.getEndExclusiveIndex()});
+				i++;
+			}
+		}
+		if(i==parts.size()) {
+			arr.add(new int[] {parts.get(i-1).getStartIndex(), parts.get(i-1).getEndExclusiveIndex()});
+		}
+		return arr.toArray(int[][]::new);
 	}
 	
 	
